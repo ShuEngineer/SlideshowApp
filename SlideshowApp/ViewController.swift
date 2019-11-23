5331bd265fab1e70bba56803ddf6e9323bdf3172
ShuEngineer <supercar110shu@gmail.com>
2019-11-23 16:42:36 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 伊藤嵩 on 2019/11/23.
+//  Copyright © 2019 Shu Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
